7dbdce1343072d38acab393212552fb80708c86b
batoolaa <abbassbatoolali@gmail.com>
2020-05-12 00:57:40 +0500

    update

--- Closures/script.js
+++ Closures/script.js
@@ -12,24 +12,38 @@ addToCartButtonsDOM.forEach(addToCartButtonDOM => {
             image: productDOM.querySelector('.product__image').getAttribute('src'),
             name: productDOM.querySelector('.product__name').innerText,
             price: productDOM.querySelector('.product__price').innerText,
-
+            quantity: 1,
         }
 
-        console.log(cart.filter(cartItem => cartItem.name === product.name).length);
+        const isInCart = cart.filter(cartItem => cartItem.name === product.name).length > 0;
+        if(!isInCart){
 
-        cartDOM.insertAdjacentHTML('beforeend', `
-        <div class='cart__item'>
-            <img class='cart__item__image' src=${product.image}>
-            <h1 class='cart__item__name'>${product.name}</h1>
-            <h3 class='cart__item__price'>${product.price}<h3>
-        </div>
-        `);
+            cartDOM.insertAdjacentHTML('beforeend', `
+                <div class="cart__item">
+                    <img class="cart__item__image" src=${product.image}>
+                    <h3 class="cart__item__name">${product.name}</h3>
+                    <h3 class="cart__item__price">${product.price}<h3>
+                    <button class="btn btn--primary btn-small" data-action="DECREASE_ITEM">&minus;</button>
+                    <h3 class='cart__item__quantity'>${product.quantity}</h3>
+                    <button class="btn btn--primary btn-small" data-action="INCREASE_ITEM">&plus;</button>
+                    <button class="btn btn--danger btn-small" data-action="DECREASE_ITEM">&times</button>
+                </div>
+            `);
 
-        cart.push(product);
-        addToCartButtonDOM.innerText = 'In Cart';
+            cart.push(product);
+            addToCartButtonDOM.innerText = 'In Cart';
 
+            const cartItemsDOM = cartDOM.querySelectorAll('.cart__item');
+            cartItemsDOM.forEach(cartItemDOM => {
+                cartItemDOM.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', () => {
+                    console.log(cartItemDOM);
+                    console.log(product.name);
 
-    })
+                });
+            });
+        }
+
+    });
     
 })
 
